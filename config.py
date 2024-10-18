2baa34cd629d331d2d678e5ab992eddf21cc68d4
UKI-ADMIN02 <kajubass.com>
2024-10-18 20:33:04 +0530

    second commit

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-# Configuration settings
 WINDOW_TITLE = "File Organizer"
 WINDOW_SIZE = "800x600"
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -11,9 +11,7 @@ def organize_files(source_folder):
             file_extension = filename.split('.')[-1]
             destination_folder = os.path.join(source_folder, file_extension)
 
-            # Create the folder if it doesn't exist
             if not os.path.exists(destination_folder):
                 os.makedirs(destination_folder)
 
-            # Move the file to the corresponding folder
             shutil.move(os.path.join(source_folder, filename), os.path.join(destination_folder, filename))
